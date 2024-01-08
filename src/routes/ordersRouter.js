b9b1bd628215f63416c1499a7d3bc983f35e060d
SarunasT <sarunas.tarvydas@hotmail.com>
2024-01-08 08:22:26 +0100

    orders routes work

--- src/routes/ordersRouter.js
+++ src/routes/ordersRouter.js
@@ -38,5 +38,59 @@ ordersRouter.post('/', checkOrder, async (req, res) => {
   res.json(newOrderRezObj);
 });
 
+// GET /api/orders - gauti visa pilna orders plius user_name, shop_item_name ir shop_item_price
+
+ordersRouter.get('/', async (req, res) => {
+  const sql = `SELECT orders.order_id, orders.user_id, users.user_name, orders.shop_item_id, shop_items.shop_item_name, orders.quantity, shop_items.price, orders.total_price, orders.status FROM orders JOIN users ON orders.user_id = users.user_id JOIN shop_items ON orders.shop_item_id = shop_items.shop_item_id`;
+  const [rows, error] = await dbQueryWithData(sql);
+  if (error) {
+    res.status(500).json({ error: 'Internal server error' });
+    return;
+  }
+  res.json(rows);
+});
+
+// GET /api/orders/:orderId - gauti viviena order pagal order id
+
+ordersRouter.get('/:orderId', async (req, res) => {
+  const orderId = req.params.orderId;
+  const sql = `SELECT orders.order_id, orders.user_id, users.user_name, orders.shop_item_id, shop_items.shop_item_name, orders.quantity, shop_items.price, orders.total_price, orders.status FROM orders JOIN users ON orders.user_id = users.user_id JOIN shop_items ON orders.shop_item_id = shop_items.shop_item_id`;
+  const [rows, error] = await dbQueryWithData(sql, [orderId]);
+  if (error) {
+    res.status(500).json({ error: 'Internal server error' });
+    return;
+  }
+  res.json(rows[0]);
+});
+
+// GET /api/orders/:orderId - gauti visus pilnus orders pagal user id
+
+ordersRouter.get('/user/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  const sql = `SELECT 
+  orders.order_id,
+  orders.user_id,
+  users.user_name,
+  orders.shop_item_id,
+  shop_items.shop_item_name,
+  orders.quantity,
+  shop_items.price,
+  orders.total_price,
+  orders.status
+FROM 
+  orders
+JOIN 
+  users ON orders.user_id = users.user_id
+JOIN 
+  shop_items ON orders.shop_item_id = shop_items.shop_item_id
+WHERE users.user_id=?`;
+  const [rows, error] = await dbQueryWithData(sql, [userId]);
+  if (error) {
+    res.status(500).json({ error: 'Internal server error' });
+    return;
+  }
+  res.json(rows);
+});
+
 // export routers
 module.exports = ordersRouter;
